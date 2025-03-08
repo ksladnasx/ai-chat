2315c8c651aa14e57c94afb750f0297a168fe3f8
ksldnasx <wh8261408@126.com>
2025-03-08 16:53:19 +0800

    11

--- src/router/index.ts
+++ src/router/index.ts
@@ -34,7 +34,7 @@ const router = createRouter({
 })
 
 // 路由守卫
-router.beforeEach((to, _from, next) => {
+router.beforeEach ((to, _from, next) => {
   const isAuthenticated = localStorage.getItem('isLogin')
   
   if (to.meta.requiresAuth && !isAuthenticated) {
